cf9495276038d09704348f3e013e4341ca1a11f4
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-02-01 09:43:29 +0200

    Finished Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import './App.css';
-// import Home from './components/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
   return (
-    <div className="App">
+    <>
+      <Navbar />
 
-
-
-    </div>
+    </>
   );
 }
 
