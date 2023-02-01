47818cc5d56e8be94be07a6d346b32ea46e59356
omar kouzi <omarkouzi81248@gmail.com>
2023-02-01 22:44:09 +0000

    working on projects section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 // import Home from './components/Home';
 import { AdminView } from './Components/AdminView/AdminView';
 import {Header} from './Components/Header/Header'
-
+import { Languages } from './Components/Languages/Languages';
 function App() {
   return (
     <div className="App">
     <Header></Header>
+    <Languages></Languages>
+
+
     <AdminView></AdminView>
 
     </div>
---
+++ frontend/src/Components/Languages/Languages.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Languages.css"
+export const Languages = () => {
+    return <div class="background">
+        <div class="languages">
+        <p class="language"><img src="" alt="img.png" /></p>        
+        <p class="language"><img src="" alt="img.png" /></p>        
+        <p class="language"><img src="" alt="img.png" /></p>        
+        <p class="language"><img src="" alt="img.png" /></p>        
+        <p class="language"><img src="" alt="img.png" /></p>        
+        <p class="language"><img src="" alt="img.png" /></p>        
+      
+</div>
+<div class="lines2">
+       <div class="line"></div>
+       <div class="line"></div>        
+       </div>
+    </div>
+}
