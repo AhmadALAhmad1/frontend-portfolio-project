19c4d554af2f6edea0f0ce7991ef8eee8fe84612
linarawwas <linamkraw2@gmail.com>
2023-02-02 16:25:02 +0100

    before using state and hooks

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,49 +1,53 @@
 import React from "react";
 import profileIcon from '../../images/profileIcon.ico'
 import style from './Dashboard.css'
-import profilePicture from '../../images/placeholderImage.jpg'
+import profilePicture from '../../images/cat.jpg'
+import mobileMenuIcon from  '../../images/mobileMenu.png'
+
 function Dashboard(){
     return(
         <div>
             <nav  className="DashNav">
-            <h1>Dashboard</h1>
+              <h1>Dashboard</h1>
             <div className="navLeft">
                 <img src={profileIcon} alt="" />
-                <button>Log out</button>
+                <button className="logOut">Log out</button>
             </div>
             </nav>
             <div className="DashBody">
                 <div className="aside">
-                    <p className="asideElement">Profile</p>
-                    <p className="asideElement">Statistics</p>
-                    <p className="asideElement">Mail box</p>
-                    <p className="asideElement">About Me</p>
-                    <p className="asideElement">Skills</p>
-                    <p className="asideElement">My Work</p>
-                    <p className="asideElement">Social Media</p>
-
+                    <a href="#" id='profile' className="asideElement" >Profile</a>
+                    <a href="#" id="header" className="asideElement">Header</a>
+                    <a href="#" id="contactInfo" className="asideElement">Contact Info</a>
+                    <a href="#" id="about" className="asideElement">About</a>
+                    <a href="#" id='skills' className="asideElement">Skills</a>
+                    <a href="#" id='myWork' className="asideElement">My Work</a>
+                    <a href="#" id='socialMediaLinks' className="asideElement">Social Media Links</a>
                 </div>
-                <div className="infoSection">
+                <div id='infoSectionProfile' className="infoSectionProfile">
                         <div className="input">
-                            <label for='firstName'>firstName</label><input  type="text" placeholder=""  id="firstName" className="asideElement" Profile/>
+                            <label for='firstName'> First Name</label><input  type="text" placeholder=""  id="firstName" className="asideElement" Profile/>
                         </div>
                         <div className="input">
-                            <label for='lastName' >lastName </label><input  type="text" placeholder="" id="lastName" className="asideElement"Statistics/>       
+                            <label for='lastName' >Last Name </label><input  type="text" placeholder="" id="lastName" className="asideElement"Statistics/>       
                         </div>
                         <div className="input">
-                        <label for='headLine'>headLine</label><input  type="text" placeholder="" id="headLine" className="asideElement"Mail box/>      
+                        <label for='headLine'>Head-line</label><input  type="text" placeholder="" id="headLine" className="asideElement"Mail box/>      
                         </div>
                         <div className="input">
-                        <label for='newPass'>newPass</label><input  type="text" placeholder="" id="newPass" className="asideElement"About Me/>       
+                        <label for='newPass'>New Password</label><input  type="text" placeholder="" id="newPass" className="asideElement"About Me/>       
                         </div>
                         <div className="input">
-                        <label for='confirmPass'>confirmPass</label><input  type="text" placeholder="" id="confirmPass" className="asideElement"Skill/>
+                        <label for='confirmPass'>Confirm Password</label><input  type="text" placeholder="" id="confirmPass" className="asideElement"Skill/>
                         </div>
                     <button className="save">SAVE</button>
                 </div>
-                <div className="profilePictureSection">
+                <div id='pictureSectionProfile' className="pictureSectionProfile">
+                    <div className="imgSection">
                     <img src={profilePicture} alt="" />
                     <p className="pinkLine">LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL</p>
+                    </div>
+                    
                <button className="upload">Upload</button> 
                </div>
 
---
+++ src/Components/Dashboard/dashboard.js
@@ -0,0 +1,6 @@
+
+ 
+document.getElementById("profile").onclick = function() {
+    document.getElementById("infoSectionProfile").style.display = "block";
+    document.getElementById("pictureSectionProfile").style.display = "flex";
+}
