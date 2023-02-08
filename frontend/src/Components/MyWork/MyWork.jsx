cc549dbd74fa54932dea9514dd16ddf42511eb4a
omar kouzi <omarkouzi81248@gmail.com>
2023-02-08 12:06:15 +0000

    finishing my part of frontend

--- frontend/src/Components/MyWork/MyWork.jsx
+++ frontend/src/Components/MyWork/MyWork.jsx
@@ -1,48 +1,43 @@
 import React from "react";
 import"./MyWork.css"
 export const MyWork = () =>{
-    return  <div class="main">
-            <h1 class="myWorkTitle">My Work</h1>
-            <div class="myWorkline"></div>
-            <div class="conteudo">
-                <div class="gallery">
-               
-                    <div>
-                        <article class="card">
-                            <img src="" alt="a"/>
-                        </article>
-                        <h3 class="projectName">project-name</h3>
-                    </div> 
-                    <div>
-                        <article class="card">
-                            <img src="" alt="a"/>
-                        </article>
-                        <h3 class="projectName">project-name</h3>
-                    </div> 
-                    <div>
-                        <article class="card">
-                            <img src="" alt="a"/>
-                        </article>
-                        <h3 class="projectName">project-name</h3>
-                    </div> 
-                    <div>
-                        <article class="card">
-                            <img src="" alt="a"/>
-                        </article>
-                        <h3 class="projectName">project-name</h3>
-                    </div> 
-                    <div>
-                        <article class="card">
-                            <img src="" alt="a"/>
-                        </article>
-                        <h3 class="projectName">project-name</h3>
-                    </div> 
-                    
+    return  <div id="mywork">
+                <h1 class="myWorkTitle">My Work</h1>
+                <div class="myWorkline"></div>
+                <div class="conteudo">
+                    <div class="gallery">
 
-                      
-                   </div>
+                        <div>
+                            <article class="card">
+                                <img src="" alt="a"/>
+                            </article>
+                            <h3 class="projectName">project-name</h3>
+                        </div> 
+                        <div>
+                            <article class="card">
+                                <img src="" alt="a"/>
+                            </article>
+                            <h3 class="projectName">project-name</h3>
+                        </div> 
+                        <div>
+                            <article class="card">
+                                <img src="" alt="a"/>
+                            </article>
+                            <h3 class="projectName">project-name</h3>
+                        </div> 
+                        <div>
+                            <article class="card">
+                                <img src="" alt="a"/>
+                            </article>
+                            <h3 class="projectName">project-name</h3>
+                        </div> 
+                        <div>
+                            <article class="card">
+                                <img src="" alt="a"/>
+                            </article>
+                            <h3 class="projectName">project-name</h3>
+                        </div>          
+                    </div>
                 </div>
-
-
             </div>
 }   
