ed3c0fed1f4be185d1c7807576c53a1aeb185d0c
omar kouzi <omarkouzi81248@gmail.com>
2023-02-11 15:34:35 +0000

    kind of finishing the design

--- frontend/src/Components/MyWork/ProjectCard/ProjectCard.jsx
+++ frontend/src/Components/MyWork/ProjectCard/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard(props)
   return (
     <div class="fullCard">
       <article class="card">
-        <img src={props.img} alt="a" />
+        <img src={props.img} alt="a" className="cardImg"/>
       </article>
       <h3 class="projectName">{props.name}</h3>
     </div>
