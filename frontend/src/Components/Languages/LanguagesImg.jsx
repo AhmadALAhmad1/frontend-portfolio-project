b2393efbf717d2e2fdaa60af5ebbbe1c31575b10
linarawwas <linamkraw2@gmail.com>
2023-02-23 02:55:51 +0100

    unnecessary stuff

--- frontend/src/Components/Languages/LanguagesImg.jsx
+++ frontend/src/Components/Languages/LanguagesImg.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './Languages.css'
 
 export default function LanguagesImg(props){
+    const styles={
+        width:"200px", 
+        height:"auto" 
+    }
 return(
     <>
-        <img className='languageImg' src={props.image} alt="languages" />
+        <img className='languageImg' style={styles} src={props.image} alt="languages" />
     </>
 )
 }
